390cabc0f58e9ff0401c45284f5966ad0db1668d
Laura Brooks <laura.brooks0@outlook.com>
2022-01-13 22:57:58 +0900

    all major routing done. styling TBD. fix show page TBD. need to add search.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+puts 'Delete flats...'
+Flat.destroy_all
+
+ 10.times do |flat|
+  Flat.create(
+    name: Faker::Emotion.adjective + " Flat",
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.sentence(word_count: 12),
+    price_per_night: Faker::Number.within(range: 50..150),
+    number_of_guests: Faker::Number.within(range: 2..8)
+  )
+end
+puts "Generated flats!"
